3f6f5fe58dd63b059df82b790a38b1f1d5d4e0e2
bconti123 <bconti00@gmail.com>
2023-03-16 00:37:46 -0700

    Add  and

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
 
 const $submitForm = $('#submitForm');
+const $submitSection = $('#submit-section');
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
