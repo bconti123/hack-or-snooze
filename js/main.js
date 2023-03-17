d9b980c2f9a8d2f9c4ccde1de60fb28c5f630027
bconti123 <bconti00@gmail.com>
2023-03-17 13:33:26 -0700

    Fixed issues on

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const $navLogin = $("#nav-login");
 const $navUserProfile = $("#nav-user-profile");
 const $navLogOut = $("#nav-logout");
 
-const $submitForm = $('#submitForm');
+const $submitForm = $('#submit-story');
 const $submitSection = $('#submit-section');
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
